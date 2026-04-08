c4c50169f63aa057c3b7c9be3ad31cc0bdcc3b15
WALEED SIDDIG MOHAMED SIDDIG <TP078784@mail.apu.edu.my>
2026-04-08 15:08:40 +0800

    Admin waleed create all this missing thing and thank you all

---
+++ AsTest1/Forms/Admin/AdminTrainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace APUCC_Project.Forms.Admin
+{
+    public partial class AdminTrainer : Form
+    {
+        public AdminTrainer()
+        {
+            InitializeComponent();
+        }
+    }
+}
