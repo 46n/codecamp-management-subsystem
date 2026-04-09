897ec1b01879e9627272135703ed034dd3337827
ABDULAZIZ ABDULLAH MOHAMMED QADERI <TP091283@mail.apu.edu.my>
2026-04-09 22:48:06 +0800

    BaseShellForm design structure 2nd

--- AsTest1/Forms/Student/StudentShellForm.Designer.cs
+++ AsTest1/Forms/Student/StudentShellForm.Designer.cs
@@ -20,83 +20,34 @@
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // leftBorderBtn
-            // 
-            leftBorderBtn.Margin = new Padding(2, 1, 2, 1);
-            leftBorderBtn.Size = new Size(4, 23);
-            // 
-            // panelMenu
-            // 
-            panelMenu.Location = new Point(10, 8);
-            panelMenu.Margin = new Padding(5, 3, 5, 3);
-            panelMenu.Size = new Size(79, 142);
-            // 
             // homebtn
             // 
             homebtn.FlatAppearance.BorderSize = 0;
-            homebtn.Location = new Point(0, 28);
-            homebtn.Margin = new Padding(1, 0, 1, 0);
-            homebtn.Padding = new Padding(5, 0, 0, 0);
-            homebtn.Size = new Size(79, 15);
             homebtn.Text = "Main";
-            // 
-            // panelLogo
-            // 
-            panelLogo.Margin = new Padding(1, 0, 1, 0);
-            panelLogo.Size = new Size(79, 28);
-            // 
-            // iconButton4
-            // 
-            iconButton4.FlatAppearance.BorderSize = 0;
-            iconButton4.Location = new Point(0, 88);
-            iconButton4.Margin = new Padding(1, 0, 1, 0);
-            iconButton4.Padding = new Padding(5, 0, 0, 0);
-            iconButton4.Size = new Size(79, 15);
-            // 
-            // iconButton3
-            // 
-            iconButton3.FlatAppearance.BorderSize = 0;
-            iconButton3.Location = new Point(0, 58);
-            iconButton3.Margin = new Padding(1, 0, 1, 0);
-            iconButton3.Padding = new Padding(5, 0, 0, 0);
-            iconButton3.Size = new Size(79, 15);
             // 
             // iconButton2
             // 
             iconButton2.FlatAppearance.BorderSize = 0;
-            iconButton2.Location = new Point(0, 43);
-            iconButton2.Margin = new Padding(1, 0, 1, 0);
-            iconButton2.Padding = new Padding(5, 0, 0, 0);
-            iconButton2.Size = new Size(79, 15);
             // 
-            // pictureBox1
+            // iconButton3
             // 
-            pictureBox1.Location = new Point(-4, -3);
-            pictureBox1.Margin = new Padding(1, 0, 1, 0);
-            pictureBox1.Size = new Size(77, 34);
+            iconButton3.FlatAppearance.BorderSize = 0;
+            // 
+            // iconButton4
+            // 
+            iconButton4.FlatAppearance.BorderSize = 0;
             // 
             // Profile
             // 
             Profile.FlatAppearance.BorderSize = 0;
-            Profile.Location = new Point(0, 73);
-            Profile.Margin = new Padding(1, 0, 1, 0);
-            Profile.Padding = new Padding(5, 0, 0, 0);
-            Profile.Size = new Size(79, 15);
-            // 
-            // MainPanel
-            // 
-            MainPanel.Margin = new Padding(1, 0, 1, 0);
-            MainPanel.Size = new Size(210, 198);
             // 
             // StudentShellForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(414, 198);
-            Location = new Point(0, 0);
-            Margin = new Padding(1, 0, 1, 0);
-            MaximumSize = new Size(416, 200);
-            MinimumSize = new Size(416, 200);
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(770, 425);
+            MaximumSize = new Size(770, 425);
+            MinimumSize = new Size(770, 425);
+            StartPosition = FormStartPosition.CenterScreen;
             Name = "StudentShellForm";
             Load += StudentShellForm_Load;
             panelMenu.ResumeLayout(false);
