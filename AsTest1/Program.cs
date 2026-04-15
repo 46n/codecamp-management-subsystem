d194e86159dea37724ff8a30695b9810f82a39d1
Abdalla Hassan <abdalla31.31@hotmail.com>
2026-04-15 11:15:32 +0800

    Login Form has been published successfully

--- AsTest1/Program.cs
+++ AsTest1/Program.cs
@@ -1,3 +1,4 @@
+using APUCC_Project.Forms.Common;
 using APUCC_Project.Forms.Trainer;
 
 namespace APUCC_Project
@@ -16,7 +17,8 @@ namespace APUCC_Project
             //Application.Run(new StudentShellForm());
             //Application.Run(new LecturerShellForm());
             //Application.Run(new TrainerShellForm());
-            Application.Run(new AdminShellForm());
+            //Application.Run(new AdminShellForm());
+            Application.Run(new LoginForm());
         }
     }
 }
