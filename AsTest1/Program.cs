e95b1ddbb86247ffcef0795f25a6b457cb21c3a5
WALEED SIDDIG MOHAMED SIDDIG <TP078784@mail.apu.edu.my>
2026-04-16 22:29:59 +0800

    small change in admin

--- AsTest1/Program.cs
+++ AsTest1/Program.cs
@@ -1,3 +1,5 @@
+using APUCC_Project.Forms.Common;
+
 namespace APUCC_Project
 {
     internal static class Program
@@ -12,8 +14,9 @@ namespace APUCC_Project
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             //Application.Run(new StudentShellForm());
-            Application.Run(new StudentShellForm(1));
-            //Application.Run(new TrainerShellForm());
+            //Application.Run(new StudentShellForm(1));
+            Application.Run(new AdminShellForm());
+            Application.Run(new LoginForm());
         }
     }
 }
